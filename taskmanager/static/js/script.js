8be8440ed9cf3bb5ade04a38dd4a9279f4569157
Whon1980-DL <supitpongw@hotmail.com>
2025-02-16 15:09:26 +0000

    Add functionality add task

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,9 +2,22 @@ document.addEventListener('DOMContentLoaded', function() {
     // sidenav initialization
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    // modal box for delete category confirmation
+    let modals = document.querySelectorAll('.modal');
+    M.Modal.init(modals);
+
+    // datepicker initialization
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmm, yyyy",
+        i18n: {done: "Select"}
+    });
+
+    // select initialization
+    var selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+   
   });
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems);
-  });
+  
